cca4fb5464459b58be06197625cdbdffe3ead6c0
sami1793 <sami_17_94@hotmail.com>
2021-07-02 00:47:08 -0300

    Se creo endpoint POST/categorias en CategoriaController que crea una categoria.

--- src/main/java/ar/com/ada/api/questionados/controllers/CategoriaController.java
+++ src/main/java/ar/com/ada/api/questionados/controllers/CategoriaController.java
@@ -1,5 +1,29 @@
 package ar.com.ada.api.questionados.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import ar.com.ada.api.questionados.entities.Categoria;
+import ar.com.ada.api.questionados.models.response.GenericResponse;
+import ar.com.ada.api.questionados.services.CategoriaService;
+
+@RestController
 public class CategoriaController {
     
+    @Autowired
+    private CategoriaService service;
+
+    @PostMapping("/categorias")//crea una categoria
+    public ResponseEntity<?> crearCategoria(@RequestBody Categoria categoria){
+
+        GenericResponse respuesta = new GenericResponse();
+        service.crearCategoria(categoria);
+
+        respuesta.isOk=true;
+        respuesta.message="Categoria creada con éxito";
+
+        return ResponseEntity.ok(respuesta);
+
+    } 
 }
--- src/main/java/ar/com/ada/api/questionados/services/CategoriaService.java
+++ src/main/java/ar/com/ada/api/questionados/services/CategoriaService.java
@@ -1,5 +1,20 @@
 package ar.com.ada.api.questionados.services;
 
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.Service;
+
+import ar.com.ada.api.questionados.entities.Categoria;
+import ar.com.ada.api.questionados.repos.CategoriaRepository;
+
+@Service
 public class CategoriaService {
+
+    @Autowired
+    CategoriaRepository repo;
+
+    public void crearCategoria(Categoria categoria) {
+        repo.save(categoria);
+
+    }
     
 }
