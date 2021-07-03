0375e6106271e71e73a59e7978de354da4ff917f
sami1793 <sami_17_94@hotmail.com>
2021-07-03 19:59:26 -0300

    Se creo endpoint GET /preguntas

--- src/main/java/ar/com/ada/api/questionados/controllers/PreguntaController.java
+++ src/main/java/ar/com/ada/api/questionados/controllers/PreguntaController.java
@@ -1,5 +1,24 @@
 package ar.com.ada.api.questionados.controllers;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ar.com.ada.api.questionados.entities.Pregunta;
+
+import ar.com.ada.api.questionados.services.PreguntaService;
+
+@RestController
 public class PreguntaController {
     
+    @Autowired
+    private PreguntaService service;
+
+    @GetMapping("/preguntas")
+    public ResponseEntity<List<Pregunta>> traerPreguntas(){//trae la lista de TODAS las preguntas        
+        return ResponseEntity.ok(service.traerPreguntas());
+    }
 }
--- src/main/java/ar/com/ada/api/questionados/services/PreguntaService.java
+++ src/main/java/ar/com/ada/api/questionados/services/PreguntaService.java
@@ -1,5 +1,19 @@
 package ar.com.ada.api.questionados.services;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.com.ada.api.questionados.entities.Pregunta;
+import ar.com.ada.api.questionados.repos.PreguntaRepository;
+
+@Service
 public class PreguntaService {
-    
+    @Autowired
+    PreguntaRepository repo;
+
+    public List<Pregunta> traerPreguntas(){
+        return repo.findAll();
+    }
 }
